853bf02db5a5e88e2a67413e3b7de428e45bc501
BSC-04-20 <bsc-04-20@unima.ac.mw>
2025-03-31 00:45:10 +0200

    Resolved an id issue

--- app/Models/ResourceFile.php
+++ app/Models/ResourceFile.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class ResourceFile extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $keyType = 'string'; // UUID is a string type
     public $incrementing = false; // Disable auto-incrementing
-    
+
     protected $fillable = [
         'resource_id', 'file_path',
     ];
